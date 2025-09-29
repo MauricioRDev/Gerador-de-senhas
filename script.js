54205bfdc95310aaf7376ca3e2946ab8fc144810
Mauricio Ribeiro <mauricioribeiro618@gmail.com>
2025-09-29 11:01:16 -0300

    Incluido checkbox

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let chkMaiusculas = document.querySelector("#maiusculas");
+let chkMinusculas = document.querySelector("#minusculas");
+let chkNumeros = document.querySelector("#numeros");
+let chkSimbolos = document.querySelector("#simbolos");
+
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
@@ -6,30 +11,48 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%*';
-let novaSenha = '';
+let maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let minusculas = "abcdefghijklmnopqrstuvwxyz";
+let numeros = "0123456789";
+let simbolos = "!@#%*";
+
+let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   sizePassword.innerHTML = this.value;
-}
+};
 
+function generatePassword() {
+  let charset = '';
 
-function generatePassword(){
+  if (chkMaiusculas.checked) charset += maiusculas;
+  if (chkMinusculas.checked) charset += minusculas;
+  if (chkNumeros.checked) charset += numeros;
+  if (chkSimbolos.checked) charset += simbolos;
 
-  let pass = '';
-  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+  if (charset === '') {
+    alert(
+      "Porfavor, Selecione pelo menos uma opção para uma senha mais SEGURA!"
+    );
+    return;
+  }
+
+  let pass = "";
+  let n = charset.length;
+
+  for (let i = 0; i < sliderElement.value; i++) {
     pass += charset.charAt(Math.floor(Math.random() * n));
   }
-  
-  console.log(pass)
+
+  console.log(pass);
   containerPassword.classList.remove("hide");
   password.innerHTML = pass;
   novaSenha = pass;
 }
 
-function copyPassword(){
-  alert("Senha copiada com sucesso!")
+function copyPassword() {
+  alert("Senha copiada com sucesso!");
   navigator.clipboard.writeText(novaSenha);
 }
